31227926b39fa15dc4504b8b75fb9812a35505d7
cazmat <93198094+cazmat@users.noreply.github.com>
2024-08-07 02:15:05 -0500

    Set mod id

---
+++ src/main/java/me/cazmat/smcmod/Constants.java
@@ -0,0 +1,8 @@
+package me.cazmat.smcmod;
+
+public final class Constants {
+    protected Constants() {}
+
+    // General Definitions
+    public static final String MOD_ID = "smcmod";
+}
---
+++ src/main/java/me/cazmat/smcmod/SMCMod.java
@@ -0,0 +1,7 @@
+package me.cazmat.smcmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class SMCMod {
+}
