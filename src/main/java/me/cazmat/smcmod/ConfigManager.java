cef615e9ada30080a789d469bf7113de515d0a07
cazmat <93198094+cazmat@users.noreply.github.com>
2024-08-15 14:34:32 -0500

    Load config

--- src/main/java/me/cazmat/smcmod/ConfigManager.java
+++ src/main/java/me/cazmat/smcmod/ConfigManager.java
@@ -1,7 +1,11 @@
 package me.cazmat.smcmod;
 
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.common.Mod;
+
+import java.nio.file.Path;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = Constants.MOD_ID)
 public class ConfigManager {
@@ -11,5 +15,10 @@ public class ConfigManager {
         COMMON_BUILDER.comment(Constants.MOD_NAME);
         COMMON_BUILDER.pop();
         COMMON_CONFIG = COMMON_BUILDER.build();
+    }
+    public static void loadConfig(ForgeConfigSpec spec, Path path) {
+        CommentedFileConfig config = CommentedFileConfig.builder(path).sync().autosave().writingMode(WritingMode.REPLACE).build();
+        config.load();
+        spec.setConfig(config);
     }
 }
--- src/main/java/me/cazmat/smcmod/SMCMod.java
+++ src/main/java/me/cazmat/smcmod/SMCMod.java
@@ -1,7 +1,14 @@
 package me.cazmat.smcmod;
 
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.loading.FMLPaths;
 
 @Mod(Constants.MOD_ID)
 public class SMCMod {
+    public SMCMod() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigManager.COMMON_CONFIG);
+        ConfigManager.loadConfig(ConfigManager.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve(Constants.MOD_ID + "-common.toml"));
+    }
 }
